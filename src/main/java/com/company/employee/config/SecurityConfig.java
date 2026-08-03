c8522a066642cc4ec574ecb03ceac5a7ad0f9ce9
Ubuntu <ubuntu@ip-172-31-19-151.ec2.internal>
2026-08-03 18:04:05 +0000

    Fixing the sonarqube

--- src/main/java/com/company/employee/config/SecurityConfig.java
+++ src/main/java/com/company/employee/config/SecurityConfig.java
@@ -20,7 +20,6 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-                .csrf(csrf -> csrf.disable())
 
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/actuator/**").permitAll()
